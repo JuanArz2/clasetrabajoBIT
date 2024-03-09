ba2f9343271d5ef6439eeb2e7984a1815d1122da
Juan Ariza <160557685+JuanArz2@users.noreply.github.com>
2024-03-08 22:06:40 -0500

    Clase08-03JavaScript_SetPrettier

--- desafioJavaScript1/desafio.js
+++ desafioJavaScript1/desafio.js
@@ -1,5 +1,40 @@
-"use strict"
+"use strict";
 
-const nombreUsuario = prompt("Ingrese su Nombre")
-console.log(nombreUsuario)
+/* const nombreUsuario = prompt("Ingrese su Nombre")
+console.log(nombreUsuario) */
 
+/* var nombreUsuario = prompt('Nombre de Usuario')
+switch(hola){
+    case nombreUsuario = true:
+        document.write("¡Hola, " + nombreUsuario + "!")
+        break;
+    case nombreUsuario = false:
+        console.log("¡Hola tú!")
+        break;
+} */
+
+/* const perfil = "profesor";
+switch (perfil) {
+  case "estudiante":
+    console.log("eres un estudiante");
+    break;
+  case "profesor":
+    console.log("eres un profesor");
+    break;
+  default:
+    console.log("perfil no permitido");
+    break;
+} */
+
+var numero1 = prompt("#1");
+var numero2 = prompt("#2");
+
+numero1 = +numero1;
+numero2 = +numero2;
+
+document.write(numero1 + numero2);
+
+var nombre = prompt("#Nombre");
+var apellido = prompt("Apellido");
+
+document.write(nombre + " " + apellido);
